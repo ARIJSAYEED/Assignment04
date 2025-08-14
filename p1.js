402136947c95418e96962f4a1ce43f92890dda11
Sayeed <arijsayeed@gmail.com>
2025-08-15 00:58:12 +0600

    solved the first problem primarily

---
+++ p1.js
@@ -0,0 +1,8 @@
+function totalFine(fare) {
+    // You have to write your code here
+    let fine = fare + fare * (20 / 100) + 30;
+    return fine;
+}
+let fine = totalFine(-34);
+console.log(fine);
+
